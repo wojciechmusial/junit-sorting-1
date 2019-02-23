1e178e3ba13d690fcb0950c5bd430c6cff1f56d9
Justyna <blachutjvstyna@gmail.com>
2019-02-23 11:58:24 +0100

    Wersja1

--- src/test/java/SortingAlgorithmTest.java
+++ src/test/java/SortingAlgorithmTest.java
@@ -1,3 +1,27 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class SortingAlgorithmTest {
 
-}
+
+        private SortingAlgorithms classUnderTest;
+        @Before
+        public void setUp() throws Exception {
+            classUnderTest = new SortingAlgorithms();
+        }
+
+        @Test
+        public void testSortowaniaJB1() throws Exception {
+            double[] toSort = {5, 1, 3, 2};
+            double[] expeccted = {1, 2, 3, 5};
+
+            classUnderTest.sort(toSort, true);
+            Assert.assertArrayEquals(expeccted, toSort, 0.0001);
+
+            //Assert.assertTrue(classUnderTest.SortingAlgorithms(kod));
+
+        }
+
+
+    }
