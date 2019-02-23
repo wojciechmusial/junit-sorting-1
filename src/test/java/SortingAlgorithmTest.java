21e5f9700e5f55a5790add7d096ee03403390cee
milosz nawrat <milosznawrat@gmail.com>
2019-02-23 12:12:32 +0100

    v3

--- src/test/java/SortingAlgorithmTest.java
+++ src/test/java/SortingAlgorithmTest.java
@@ -2,7 +2,28 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SortingAlgorithmTest {
+public class SortingAlgorithmTest{
+    private SortingAlgorithms classUnderTest;
+
+    @Before
+
+    public void setUp() throws Exception {
+        classUnderTest = new SortingAgorithms();
+    }
+
+
+    @Test
+    public void testSortowaniaMN1(){
+        double[] toSort = {5,1,3,2};
+
+        double[] expected={1, 2, 3, 5};
+
+        classUnderTest.sort(toSort, true);
+
+        Assert.assertArrayEquals(expected, toSort, 0.001);
+
+    }
+
 
 
         private SortingAlgorithms classUnderTest;
